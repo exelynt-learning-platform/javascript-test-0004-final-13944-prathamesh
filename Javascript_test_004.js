530a97f06b9746b3b80045156b2d061831b55219
Prathamesh <124889386+Pra8123@users.noreply.github.com>
2026-03-14 13:48:18 +0530

    Add files via upload
    test 4

---
+++ Javascript_test_004.js
@@ -0,0 +1,22 @@
+function printPattern(n) {
+    const size = 2 * n - 1;
+    
+    for (let i = 0; i < size; i++) {
+        let row = "";
+        for (let j = 0; j < size; j++) {
+            
+            let top = i;
+            let left = j;
+            let bottom = size - 1 - i;
+            let right = size - 1 - j;
+            
+            
+            let minDistance = Math.min(top, bottom, left, right);
+            row += (n - minDistance) + " ";
+        }
+        console.log(row.trim());
+    }
+}
+
+
+printPattern(4);
